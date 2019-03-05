55cf118b0064da313d382144f0c874ae6c2baea5
CinnamonRollFoxi <48187962+CinnamonRollFoxi@users.noreply.github.com>
2019-03-04 19:49:08 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,21 @@
 const Commando = require('discord.js-commando');
 const prefix = "a+";
-const client = new Commando.Client({
+const bot = new Commando.Client({
     commandPrefix: prefix
 });
 
 //CommandFiles
 
-client.registry.registerGroup('simple', 'Simple');
-client.registry.registerCommandsIn(__dirname + '/commands');
+bot.registry.registerGroup('simple', 'Simple');
+bot.registry.registerDefaults();
+bot.registry.registerCommandsIn(__dirname + '/commands');
 
 
-client.on('ready', () => {
+bot.on('ready', () => {
     console.log('Ready for usage');
 });
 
-client.on('message', message => {
+bot.on('message', message => {
     if (message.content == 'ping') {
         message.reply('pong');
     }
