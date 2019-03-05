a083b85cc39a861e4f17f1ebbc72efaba7831743
CinnamonRollFoxi <48187962+CinnamonRollFoxi@users.noreply.github.com>
2019-03-04 19:34:55 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Commando = require('discord.js-commando');
+const client = new Commando.Client();
 const botconfig = require("./botconfig.json");
 
 
@@ -11,38 +11,6 @@ client.on('message', message => {
     if (message.content == 'ping') {
         message.reply('pong');
     }
-});
-
-client.on("message", async message => {
-    
-    if(message.auther.bot) return;
-    if(message.channel.type === "dm") return;
-    
-    let prefix = botconfig.prefix;
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0];
-    let args = messageArray.slice(1);
-    
- //Report Command
-
-if(cmd === `${prefix}report`){
-   
- //~report @user reason
-    
-    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!rUser) return message.channel.send("Couldn't find user!");
-    let reason = args.join(" ").slice(22);
-    
-    let reportEmbed = new Discord.RichEmbed()
-    .setDiscription("Report")
-    .setColor("#15f153")
-    .addField("Reported Players", `${rUser} with ID ${rUser.id}`);
-    
-    message.channel.send(reportEmbed);
-    
-    return;
-}
-    
 });
 
 
